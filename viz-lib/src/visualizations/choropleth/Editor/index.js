e2fd2a4d389742ae6901214265150759cf3cf01e
handeer <451583132@qq.com>
2020-07-11 16:04:48 +0800

    zh-cn

--- viz-lib/src/visualizations/choropleth/Editor/index.js
+++ viz-lib/src/visualizations/choropleth/Editor/index.js
@@ -6,8 +6,8 @@ import FormatSettings from "./FormatSettings";
 import BoundsSettings from "./BoundsSettings";
 
 export default createTabbedEditor([
-  { key: "General", title: "General", component: GeneralSettings },
-  { key: "Colors", title: "Colors", component: ColorsSettings },
-  { key: "Format", title: "Format", component: FormatSettings },
-  { key: "Bounds", title: "Bounds", component: BoundsSettings },
+  { key: "General", title: "通用", component: GeneralSettings },
+  { key: "Colors", title: "颜色", component: ColorsSettings },
+  { key: "Format", title: "格式化", component: FormatSettings },
+  { key: "Bounds", title: "边界", component: BoundsSettings },
 ]);
--- viz-lib/src/visualizations/counter/Editor/index.js
+++ viz-lib/src/visualizations/counter/Editor/index.js
@@ -4,6 +4,6 @@ import GeneralSettings from "./GeneralSettings";
 import FormatSettings from "./FormatSettings";
 
 export default createTabbedEditor([
-  { key: "General", title: "General", component: GeneralSettings },
-  { key: "Format", title: "Format", component: FormatSettings },
+  { key: "General", title: "通用", component: GeneralSettings },
+  { key: "Format", title: "格式化", component: FormatSettings },
 ]);
